9063b73da817a5624949771914358ba902fc13d7
jorgepiter <90303967+jorgepiter@users.noreply.github.com>
2021-09-30 23:54:57 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,37 @@
 let carril = 0
 let nivel = 0
+let Agitado = 0
 input.onButtonPressed(Button.A, function () {
-    if (carril >= 1 && nivel == 0) {
+    if (carril >= 1 && nivel == 1) {
         carril += -1
+        nivel = 0
     }
 })
-input.onGesture(Gesture.Shake, function () {
-    carril = randint(0, 4)
-})
-input.onButtonPressed(Button.B, function () {
-    if (carril <= 3 && nivel == 0) {
-        carril += 1
-    }
-})
-basic.forever(function () {
-    basic.clearScreen()
+function cambiar () {
     led.plot(carril, nivel)
     basic.pause(300)
     nivel += 1
     if (nivel == 5) {
         nivel = 0
+    }
+}
+input.onGesture(Gesture.Shake, function () {
+    carril = randint(0, 4)
+    nivel = 0
+    Agitado = 1
+})
+input.onButtonPressed(Button.B, function () {
+    if (carril <= 3 && nivel == 1) {
+        carril += 1
+        nivel = 0
+    }
+})
+basic.forever(function () {
+    if (Agitado == 1) {
+        cambiar()
+        if (nivel == 1) {
+            basic.pause(500)
+        }
+        basic.clearScreen()
     }
 })
